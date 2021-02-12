c43e3c54bc1945e310f9c16a9f6163a65a41d907
Quang Si <tranquangsidev@gmail.com>
2021-02-12 23:10:42 +0700

    first setup

---
+++ App_StyledComponents.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+
+const Title = styled.div`
+  color: red;
+  font-size: 25px;
+`;
+export default class App_StyledComponents extends Component {
+  render() {
+    return <div>
+      <Title>haha</Title>
+    </div>;
+  }
+}
